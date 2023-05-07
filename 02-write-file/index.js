027621dd59527eafbcb998733cf95fe1467076f7
akatimer <akatimer@gmail.com>
2023-05-07 16:09:16 +0300

    feat: add second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const read = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+read.on('SIGINT', () => {
+  console.log('\nПолучена команда завершения');
+  read.close();
+});
+
+function newInput() {
+
+  read.question('Введите текст: ', (input) => {
+    if (input === 'exit') {
+      console.log('Получена команда завершения');
+      read.close();
+      return;
+    }
+
+    fs.appendFile(filePath, input + '\n', (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      newInput();
+    });
+  });
+}
+
+newInput();
